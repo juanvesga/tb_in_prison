8b25fb244e403fcb2108903dc3379b5d9566886e
adriennzonnebeld <adrienn.zonnebeld@ru.nl>
2025-06-03 14:53:44 +0200

    treatment updated

--- functions/model with treatment attempt.R
+++ functions/model with treatment attempt.R
@@ -26,20 +26,19 @@ uli_ode <- odin({
     Ia * (sigma + mu) - 
     Ia * r_incar + 
     Ia_p * r_release *(1-screen_exit_a) +
-    Tr * (1-tx_succes_rate)
+    Tr * tx_duration * (1-tx_completion) * (1-tx_forgiveness)
   
   deriv(Is) <- 
     Ia * sigma - 
-    Is * (mutb + mu + self_cure + tx_initiation_delay) - 
+    Is * (mutb + mu + self_cure + (tx_initiation_delay*case_detection)) - 
     Is * r_incar + 
     Is_p * r_release *(1-screen_exit_s) 
   
   deriv(Tr) <-
-    Is * tx_initiation_delay -
-    Tr * tx_succes_rate -
-    Tr * (1-tx_succes_rate) +
-    Is_p * r_release * screen_exit_s +
-    Ia_p * r_release * screen_exit_a -
+    Is * tx_initiation_delay * case_detection -
+    Tr * tx_duration +
+    Is_p * r_release * screen_exit_s * screen_delay +
+    Ia_p * r_release * screen_exit_a * screen_delay -
     Tr * mu -
     Tr * r_incar + 
     Tr_p * r_release 
@@ -49,7 +48,8 @@ uli_ode <- odin({
     R * (imm*lambda + mu) - 
     R * r_incar + 
     R_p * r_release +
-    Tr * tx_succes_rate
+    Tr * tx_duration * tx_completion +
+    Tr * tx_duration * (1-tx_completion) * tx_forgiveness
   
   deriv(incidence) <-  
     U * lambda * fast + 
@@ -78,34 +78,34 @@ uli_ode <- odin({
     Ia_p * (sigma_p + mu) + 
     Ia * r_incar * (1-screen_entry_a) - 
     Ia_p * r_release -
-    Ia_p * screen_yearly_a +
-    Tr_p * (1-tx_succes_rate)
+    Ia_p * screen_yearly_a * screen_delay +
+    Tr_p * tx_duration * (1-tx_completion) * (1-tx_forgiveness)
   
   deriv(Is_p) <- 
     Ia_p * sigma_p - 
-    Is_p * (mutb + mu + self_cure + tx_iniation_delay_p) + 
+    Is_p * (mutb + mu + self_cure + (tx_iniation_delay_p * case_detection_p)) + 
     Is * r_incar * (1-screen_entry_s) - 
     Is_p * r_release -
-    Is_p * screen_yearly_s 
+    Is_p * screen_yearly_s * screen_delay
   
   deriv(Tr_p) <-
-    Is_p * tx_iniation_delay_p -
+    Is_p * tx_iniation_delay_p * case_detection_p -
     Tr_p * mu -
-    Tr_p * tx_succes_rate -
-    Tr_p * (1-tx_succes_rate) +
+    Tr_p * tx_duration +
     Tr * r_incar -
     Tr_p * r_release +
-    Ia * r_incar * screen_entry_a +
-    Is * r_incar * screen_entry_s +
-    Ia_p * screen_yearly_a +
-    Is_p * screen_yearly_s
+    Ia * r_incar * screen_entry_a * screen_delay +
+    Is * r_incar * screen_entry_s * screen_delay +
+    Ia_p * screen_yearly_a * screen_delay +
+    Is_p * screen_yearly_s * screen_delay
   
   deriv(R_p)  <- 
     Is_p * self_cure - 
     R_p * (imm*lambda_p + mu) + 
     R * r_incar -
     R_p * r_release +
-    Tr_p * tx_succes_rate
+    Tr_p * tx_duration * tx_completion +
+    Tr_p * tx_duration * (1-tx_completion) * tx_forgiveness
   
   
   # Output variables --------------------------------------------------------
@@ -142,29 +142,34 @@ uli_ode <- odin({
   lambda_p<- (1-mix_p*prob_inf_mix)*(beta_p * (Ia_p+Is_p)/N_p) + 
     mix_p*prob_inf_mix*(beta * (Ia+Is)/N) # force of infection prisons
   
+  
   # Model parameters (fixed) ------------------------------------------------
   
-  l_exp    <- 72              # Life expectancy
-  tb_dur    <- 3               # Duration of infectious period (years)
-  mu       <- 1/l_exp         # Background mortality rate
-  mutb     <- 0.5*(1/tb_dur)  # TB mortality rate
-  self_cure<- 0.5*(1/tb_dur)  # recovery
-  fast     <- 0.1             # Fraction fast progressing to active TB
-  slow     <- 0.0008          # Remote reactivation
-  sigma    <- 1/0.5           # symptom development (6 mo)
-  fast_p   <- 0.25            # Fraction fast progressing to active TB
-  slow_p   <- 0.0008          # Remote reactivation
-  sigma_p  <- 1/0.33          # symptom development (4 mo)
-  imm      <- 0.5             # Infectiousness decline (partial immunity)
+  l_exp    <- 70             # Life expectancy
+  tb_dur    <- 3             # Duration of infectious period (years)
+  mu       <- 1/l_exp        # Background mortality rate
+  mutb     <- 0.5*(1/tb_dur) # TB mortality rate
+  self_cure<- 0.5*(1/tb_dur) # recovery
+  fast     <- 0.1            # Fraction fast progressing to active TB
+  slow     <- 0.0008         # Remote reactivation
+  sigma    <- 1/0.5          # symptom development (6 mo)
+  fast_p   <- 0.25           # Fraction fast progressing to active TB
+  slow_p   <- 0.0008         # Remote reactivation
+  sigma_p  <- 1/0.33         # symptom development (4 mo)
+  imm      <- 0.5            # Infectiousness decline (partial immunity)
   I0       <- 1e-6
   P0       <- 250/1e5
   r_incar  <- 0.001          # rate of incarceration
   r_release<- 1/2.5          # rate of release (1/mean prison term)
-  prob_inf_mix<- 0.5       # probability of infection given short contact with external contactee
-  tx_initiation_delay <- if (time > 950) 1*0.87 else 0#       # Careseeking rate (1 year)
-  tx_iniation_delay_p <- if (time > 950) 0.5  else 0#       # Careseeking rate (1 year)
-  tx_succes_rate <- 0.65
-  
+  prob_inf_mix       <- 0.5  # probability of infection given short contact with external contactee
+  case_detection     <- if (time > 950) 0.87 else 0#    # case detection rate
+  case_detection_p   <- if (time > 950) 0.53 else 0#    # case detection rate
+  tx_initiation_delay<- if (time > 950) 1/0.21 else 0#  # treatment initiation delay
+  tx_iniation_delay_p<- if (time > 950) 1/0.27 else 0#  # treatment initiation delay
+  tx_completion      <- if (time > 950) 0.8 else 0#     # fraction of people completing treatment 
+  tx_duration        <- if (time > 950) 1/0.504 else 0# # duration treatment 184 days
+  tx_forgiveness     <- if (time > 950) 0.5 else 0#     # fraction recovering although not completing treatment
+  screen_delay       <- if (time > 950) 1/0.019 else 0# # delay of starting treatment after screening 7 days
   
   
   # External inputs ---------------------------------------------------------
